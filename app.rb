2779c07b62f6c468ef5b9dba6f8566853e856555
Guest User <Guest@Epicodus-8E.local>
2018-03-07 21:35:05 -0800

    set up files

---
+++ app.rb
@@ -0,0 +1,12 @@
+require('sinatra')
+require('sinatra/reloader')
+require('sinatra/activerecord')
+also_reload('lib/**.*.rb')
+require('./lib/user')
+require('./lib/survey')
+require('pry')
+require('pg')
+
+get('/') do
+  erb(:home)
+end
---
+++ config.ru
@@ -0,0 +1,8 @@
+require('sinatra')
+require('sinatra/reloader')
+require('sinatra/activerecord')
+also_reload('lib/**.*.rb')
+require('./lib/user')
+require('./lib/survey')
+require('pry')
+require('pg')
