c4bbb45487fc726491c89b21fc01bcd16501b379
KaitlynFujihara <30393329+KaitlynFujihara@users.noreply.github.com>
2018-03-08 07:26:11 -0800

    create survey.erb

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ also_reload('lib/**.*.rb')
 require('./lib/user')
 require('./lib/survey')
 require('pry')
-require('pg')
 
 get('/') do
   erb(:home)
@@ -21,7 +20,22 @@ get('/users') do
   erb(:users)
 end
 
-post('/users') do
+get('/survey') do
+  @surveys = Survey.all
+  erb(:survey)
+end
+
+get('/survey2') do
+  @surveys = Survey.all
+  erb(:survey2)
+end
+
+get('/survey3') do
+  @surveys = Survey.all
+  erb(:survey3)
+end
+
+post('/survey') do
   track = params.fetch('track')
   first_name = params.fetch('first_name')
   last_name = params.fetch('last_name')
@@ -29,15 +43,19 @@ post('/users') do
   github = params.fetch('github')
   @new_user = User.create({:track => track, :first_name => first_name, :last_name => last_name, :email => email, :github => github, :id => nil})
   @users = User.all
-  erb(:users)
-end
-
-get('/survey') do
-  @users = User.all
   erb(:survey)
 end
 
-post('/survey') do
-  @users = User.all
-  erb(:survey)
+post('/survey2') do
+  question1 = params.fetch('question1')
+  @question_1 = Survey.create({:question1 => question1, :id => nil})
+  @surveys = Survey.all
+  erb(:survey2)
+end
+
+post('/survey3') do
+  question3 = params.fetch('question3')
+  @question_3 = Survey.create({:question3 => question3, :id => nil})
+  @surveys = Survey.all
+  erb(:survey3)
 end
